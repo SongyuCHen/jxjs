c6164852c6918363c06dd4ad3c46f30489c7d539
SongyuCHen <363278429@qq.com>
2015-10-15 17:30:54 +0800

    add by csy

---
+++ jxjs/src/nju/software/jxjs/dao/DsrGrDao.java
@@ -0,0 +1,267 @@
+package nju.software.jxjs.dao;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import nju.software.jxjs.model.DsrGr;
+
+import org.hibernate.LockMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+
+
+/**
+ * A data access object (DAO) providing persistence and search support for
+ * DsrGr entities. Transaction control of the save(), update() and delete()
+ * operations can directly support Spring container-managed transactions or they
+ * can be augmented to handle user-managed Spring transactions. Each of these
+ * methods provides additional information for how to configure it for the
+ * desired type of transaction control.
+ * 
+ * @see software.tjspxt.data.dataobject.DsrGr
+ * @author MyEclipse Persistence Tools
+ */
+
+public class DsrGrDao extends HibernateDaoSupport {
+	private static final Logger log = LoggerFactory.getLogger(DsrGrDao.class);
+	// property constants
+	public static final String XM = "xm";
+	public static final String XB = "xb";
+	public static final String MZ = "mz";
+	public static final String JG = "jg";
+	public static final String ZY = "zy";
+	public static final String ZW = "zw";
+	public static final String WHCD = "whcd";
+	public static final String GZDW = "gzdw";
+	public static final String ZZMM = "zzmm";
+	public static final String JB = "jb";
+	public static final String SF = "sf";
+	public static final String HYQK = "hyqk";
+	public static final String JTQK = "jtqk";
+	public static final String JKQK = "jkqk";
+	public static final String SSGJ = "ssgj";
+	public static final String SFZHM = "sfzhm";
+	public static final String YB = "yb";
+	public static final String DH = "dh";
+	public static final String DZ = "dz";
+	public static final String QRJG = "qrjg";
+	public static final String QRGC = "qrgc";
+	public static final String ZZD = "zzd";
+	public static final String ZWZY = "zwzy";
+	public static final String ZJLB = "zjlb";
+
+	protected void initDao() {
+		// do nothing
+	}
+
+	public void save(DsrGr transientInstance) {
+		log.debug("saving DsrGr instance");
+		try {
+			getHibernateTemplate().saveOrUpdate(transientInstance);
+			log.debug("save successful");
+		} catch (RuntimeException re) {
+			log.error("save failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(DsrGr persistentInstance) {
+		log.debug("deleting DsrGr instance");
+		try {
+			getHibernateTemplate().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public DsrGr findById(software.tjspxt.data.dataobject.DsrGrId id) {
+		log.debug("getting DsrGr instance with id: " + id);
+		try {
+			DsrGr instance = (DsrGr) getHibernateTemplate().get(
+					"software.tjspxt.data.dataobject.DsrGr", id);
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List<DsrGr> findByExample(DsrGr instance) {
+		log.debug("finding DsrGr instance by example");
+		try {
+			List<DsrGr> results = (List<DsrGr>) getHibernateTemplate()
+					.findByExample(instance);
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+
+	public List findByProperty(String propertyName, Object value) {
+		log.debug("finding DsrGr instance with property: " + propertyName
+				+ ", value: " + value);
+		try {
+			String queryString = "from DsrGr as model where model."
+					+ propertyName + "= ?";
+			return getHibernateTemplate().find(queryString, value);
+		} catch (RuntimeException re) {
+			log.error("find by property name failed", re);
+			throw re;
+		}
+	}
+
+	public List<DsrGr> findByXm(Object xm) {
+		return findByProperty(XM, xm);
+	}
+
+	public List<DsrGr> findByXb(Object xb) {
+		return findByProperty(XB, xb);
+	}
+
+	public List<DsrGr> findByMz(Object mz) {
+		return findByProperty(MZ, mz);
+	}
+
+	public List<DsrGr> findByJg(Object jg) {
+		return findByProperty(JG, jg);
+	}
+
+	public List<DsrGr> findByZy(Object zy) {
+		return findByProperty(ZY, zy);
+	}
+
+	public List<DsrGr> findByZw(Object zw) {
+		return findByProperty(ZW, zw);
+	}
+
+	public List<DsrGr> findByWhcd(Object whcd) {
+		return findByProperty(WHCD, whcd);
+	}
+
+	public List<DsrGr> findByGzdw(Object gzdw) {
+		return findByProperty(GZDW, gzdw);
+	}
+
+	public List<DsrGr> findByZzmm(Object zzmm) {
+		return findByProperty(ZZMM, zzmm);
+	}
+
+	public List<DsrGr> findByJb(Object jb) {
+		return findByProperty(JB, jb);
+	}
+
+	public List<DsrGr> findBySf(Object sf) {
+		return findByProperty(SF, sf);
+	}
+
+	public List<DsrGr> findByHyqk(Object hyqk) {
+		return findByProperty(HYQK, hyqk);
+	}
+
+	public List<DsrGr> findByJtqk(Object jtqk) {
+		return findByProperty(JTQK, jtqk);
+	}
+
+	public List<DsrGr> findByJkqk(Object jkqk) {
+		return findByProperty(JKQK, jkqk);
+	}
+
+	public List<DsrGr> findBySsgj(Object ssgj) {
+		return findByProperty(SSGJ, ssgj);
+	}
+
+	public List<DsrGr> findBySfzhm(Object sfzhm) {
+		return findByProperty(SFZHM, sfzhm);
+	}
+
+	public List<DsrGr> findByYb(Object yb) {
+		return findByProperty(YB, yb);
+	}
+
+	public List<DsrGr> findByDh(Object dh) {
+		return findByProperty(DH, dh);
+	}
+
+	public List<DsrGr> findByDz(Object dz) {
+		return findByProperty(DZ, dz);
+	}
+
+	public List<DsrGr> findByQrjg(Object qrjg) {
+		return findByProperty(QRJG, qrjg);
+	}
+
+	public List<DsrGr> findByQrgc(Object qrgc) {
+		return findByProperty(QRGC, qrgc);
+	}
+
+	public List<DsrGr> findByZzd(Object zzd) {
+		return findByProperty(ZZD, zzd);
+	}
+
+	public List<DsrGr> findByZwzy(Object zwzy) {
+		return findByProperty(ZWZY, zwzy);
+	}
+
+	public List<DsrGr> findByZjlb(Object zjlb) {
+		return findByProperty(ZJLB, zjlb);
+	}
+
+	public List findAll() {
+		log.debug("finding all DsrGr instances");
+		try {
+			String queryString = "from DsrGr";
+			return getHibernateTemplate().find(queryString);
+		} catch (RuntimeException re) {
+			log.error("find all failed", re);
+			throw re;
+		}
+	}
+
+	public DsrGr merge(DsrGr detachedInstance) {
+		log.debug("merging DsrGr instance");
+		try {
+			DsrGr result = (DsrGr) getHibernateTemplate().merge(
+					detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(DsrGr instance) {
+		log.debug("attaching dirty DsrGr instance");
+		try {
+			getHibernateTemplate().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(DsrGr instance) {
+		log.debug("attaching clean DsrGr instance");
+		try {
+			getHibernateTemplate().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public static DsrGrDao getFromApplicationContext(ApplicationContext ctx) {
+		return (DsrGrDao) ctx.getBean("DsrGrDAO");
+	}
+}
---
+++ jxjs/src/nju/software/jxjs/dao/DsrJbDao.java
@@ -0,0 +1,263 @@
+package nju.software.jxjs.dao;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import nju.software.jxjs.model.DsrJb;
+
+import org.hibernate.LockMode;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+/**
+ * A data access object (DAO) providing persistence and search support for
+ * DsrJb entities. Transaction control of the save(), update() and delete()
+ * operations can directly support Spring container-managed transactions or they
+ * can be augmented to handle user-managed Spring transactions. Each of these
+ * methods provides additional information for how to configure it for the
+ * desired type of transaction control.
+ * 
+ * @see software.tjspxt.data.dataobject.DsrJb
+ * @author MyEclipse Persistence Tools
+ */
+
+public class DsrJbDao extends HibernateDaoSupport {
+	private static final Logger log = LoggerFactory.getLogger(DsrJbDao.class);
+	// property constants
+	public static final String DSRSSDW = "dsrssdw";
+	public static final String DSRLB = "dsrlb";
+	public static final String DSRJC = "dsrjc";
+	public static final String SFSSDBR = "sfssdbr";
+	public static final String DSRBGFS = "dsrbgfs";
+	public static final String DSRBGYY = "dsrbgyy";
+	public static final String SFSA = "sfsa";
+	public static final String SFSG = "sfsg";
+	public static final String SFSQ = "sfsq";
+	public static final String SFST = "sfst";
+	public static final String SFSW = "sfsw";
+	public static final String QQPCJE = "qqpcje";
+	public static final String SCBC = "scbc";
+	public static final String HPJE = "hpje";
+
+	protected void initDao() {
+		// do nothing
+	}
+
+	public void save(DsrJb transientInstance) {
+		log.debug("saving DsrJb instance");
+		try {
+			getHibernateTemplate().saveOrUpdate(transientInstance);
+			log.debug("save successful");
+		} catch (RuntimeException re) {
+			log.error("save failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(DsrJb persistentInstance) {
+		log.debug("deleting DsrJb instance");
+		try {
+			getHibernateTemplate().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public DsrJb findById(software.tjspxt.data.dataobject.DsrJbId id) {
+		log.debug("getting DsrJb instance with id: " + id);
+		try {
+			DsrJb instance = (DsrJb) getHibernateTemplate().get(
+					"software.tjspxt.data.dataobject.DsrJb", id);
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List<DsrJb> findByExample(DsrJb instance) {
+		log.debug("finding DsrJb instance by example");
+		try {
+			List<DsrJb> results = (List<DsrJb>) getHibernateTemplate()
+					.findByExample(instance);
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+
+	public List findByProperty(String propertyName, Object value) {
+		log.debug("finding DsrJb instance with property: " + propertyName
+				+ ", value: " + value);
+		try {
+			String queryString = "from DsrJb as model where model."
+					+ propertyName + "= ?";
+			return getHibernateTemplate().find(queryString, value);
+		} catch (RuntimeException re) {
+			log.error("find by property name failed", re);
+			throw re;
+		}
+	}
+
+	public List<DsrJb> findByDsrssdw(Object dsrssdw) {
+		return findByProperty(DSRSSDW, dsrssdw);
+	}
+
+	public List<DsrJb> findByDsrlb(Object dsrlb) {
+		return findByProperty(DSRLB, dsrlb);
+	}
+
+	public List<DsrJb> findByDsrjc(Object dsrjc) {
+		return findByProperty(DSRJC, dsrjc);
+	}
+
+	public List<DsrJb> findBySfssdbr(Object sfssdbr) {
+		return findByProperty(SFSSDBR, sfssdbr);
+	}
+
+	public List<DsrJb> findByDsrbgfs(Object dsrbgfs) {
+		return findByProperty(DSRBGFS, dsrbgfs);
+	}
+
+	public List<DsrJb> findByDsrbgyy(Object dsrbgyy) {
+		return findByProperty(DSRBGYY, dsrbgyy);
+	}
+
+	public List<DsrJb> findBySfsa(Object sfsa) {
+		return findByProperty(SFSA, sfsa);
+	}
+
+	public List<DsrJb> findBySfsg(Object sfsg) {
+		return findByProperty(SFSG, sfsg);
+	}
+
+	public List<DsrJb> findBySfsq(Object sfsq) {
+		return findByProperty(SFSQ, sfsq);
+	}
+
+	public List<DsrJb> findBySfst(Object sfst) {
+		return findByProperty(SFST, sfst);
+	}
+
+	public List<DsrJb> findBySfsw(Object sfsw) {
+		return findByProperty(SFSW, sfsw);
+	}
+
+	public List<DsrJb> findByQqpcje(Object qqpcje) {
+		return findByProperty(QQPCJE, qqpcje);
+	}
+
+	public List<DsrJb> findByScbc(Object scbc) {
+		return findByProperty(SCBC, scbc);
+	}
+
+	public List<DsrJb> findByHpje(Object hpje) {
+		return findByProperty(HPJE, hpje);
+	}
+
+	public List findAll() {
+		log.debug("finding all DsrJb instances");
+		try {
+			String queryString = "from DsrJb";
+			return getHibernateTemplate().find(queryString);
+		} catch (RuntimeException re) {
+			log.error("find all failed", re);
+			throw re;
+		}
+	}
+
+	public DsrJb merge(DsrJb detachedInstance) {
+		log.debug("merging DsrJb instance");
+		try {
+			DsrJb result = (DsrJb) getHibernateTemplate().merge(
+					detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(DsrJb instance) {
+		log.debug("attaching dirty DsrJb instance");
+		try {
+			getHibernateTemplate().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(DsrJb instance) {
+		log.debug("attaching clean DsrJb instance");
+		try {
+			getHibernateTemplate().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+	
+	public long getMaxDsrjbbh(long ajxh) {
+		String hql = "select max(dsrbh) from DsrJb where ajxh="+ajxh;
+		
+		Session s = this.getSession();
+		Query query = s.createQuery(hql);
+
+		int maxbh = 0;
+		if (query.uniqueResult() != null)
+			maxbh = (Integer) query.uniqueResult();
+		
+		//�ͷ���ݿ�����
+		this.releaseSession(s);
+		
+		return maxbh;
+	}
+
+	public static DsrJbDao getFromApplicationContext(ApplicationContext ctx) {
+		return (DsrJbDao) ctx.getBean("DsrJbDAO");
+	}
+	
+
+	/**
+	 * ͨ�������Լ������˵����ϵ�λ�õ���������Ϣ
+	 * @param ajxh �������
+	 * @return ��������Ϣ�б� 
+	 */
+	public List<DsrJb> getDsrbyAjxhAndDsrssdw(int ajxh)
+	{
+		String hql = "from DsrJb where ajxh = " + ajxh + " and (dsrssdw in ('12', '16', '22', '27'))";
+		try {
+			log.debug("getDsrbyAjxh successful");
+			return (List<DsrJb>) getHibernateTemplate().find(hql);
+		} catch (RuntimeException re) {
+			log.error("getDsrbyAjxh failed", re);
+			throw re;
+		}
+	}
+	
+	public List<DsrJb> getDsrByAjxh(int ajxh)
+	{
+		String hql = "from DsrJb where ajxh = " + ajxh + " ORDER BY dsrssdw, dsrbh";
+		try {
+			log.debug("getDsrbyAjxh successful");
+			return (List<DsrJb>) getHibernateTemplate().find(hql);
+		} catch (RuntimeException re) {
+			log.error("getDsrbyAjxh failed", re);
+			throw re;
+		}
+	}
+}
