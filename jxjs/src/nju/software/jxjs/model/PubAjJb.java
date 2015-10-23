59f9f1e807bfa40faaf33c67f3950ba95c2cd37b
SongyuCHen <363278429@qq.com>
2015-10-23 17:20:12 +0800

    修改Bug

--- jxjs/src/nju/software/jxjs/model/PubAjJb.java
+++ jxjs/src/nju/software/jxjs/model/PubAjJb.java
@@ -12,7 +12,7 @@ public class PubAjJb implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 	//案件序号
-	private int ajxh;
+	private Integer ajxh;
 	//案号
 	private String ah;
 	//案件名称
@@ -38,9 +38,9 @@ public class PubAjJb implements Serializable
 	//是否外地移交案件
 	private String sfwdyj;
 	//审限
-	private int sx;
+	private Integer sx;
 	//附加审限
-	private int fjsx;
+	private Integer fjsx;
 	//来源地区 GB2260-91
 	private String lydq;
 	//办案审判庭 USR206-99
@@ -88,7 +88,7 @@ public class PubAjJb implements Serializable
 	//归档日期
 	private Date gdrq;
 	//归档序列号
-	private int gdxlh;
+	private Integer gdxlh;
 	//承办人报批日期
 	private Date cbrbprq;
 	//结案人
@@ -132,564 +132,679 @@ public class PubAjJb implements Serializable
 	{
 	}
 
-	public int getAjxh()
-	{
+
+
+	public Integer getAjxh() {
 		return ajxh;
 	}
 
-	public void setAjxh(int ajxh)
-	{
+
+
+	public void setAjxh(Integer ajxh) {
 		this.ajxh = ajxh;
 	}
 
-	public String getAh()
-	{
+
+
+	public String getAh() {
 		return ah;
 	}
 
-	public void setAh(String ah)
-	{
+
+
+	public void setAh(String ah) {
 		this.ah = ah;
 	}
 
-	public String getAjmc()
-	{
+
+
+	public String getAjmc() {
 		return ajmc;
 	}
 
-	public void setAjmc(String ajmc)
-	{
+
+
+	public void setAjmc(String ajmc) {
 		this.ajmc = ajmc;
 	}
 
-	public String getAjxz()
-	{
+
+
+	public String getAjxz() {
 		return ajxz;
 	}
 
-	public void setAjxz(String ajxz)
-	{
+
+
+	public void setAjxz(String ajxz) {
 		this.ajxz = ajxz;
 	}
 
-	public String getAjly()
-	{
+
+
+	public String getAjly() {
 		return ajly;
 	}
 
-	public void setAjly(String ajly)
-	{
+
+
+	public void setAjly(String ajly) {
 		this.ajly = ajly;
 	}
 
-	public String getSpcx()
-	{
+
+
+	public String getSpcx() {
 		return spcx;
 	}
 
-	public void setSpcx(String spcx)
-	{
+
+
+	public void setSpcx(String spcx) {
 		this.spcx = spcx;
 	}
 
-	public String getSpcxdz()
-	{
+
+
+	public String getSpcxdz() {
 		return spcxdz;
 	}
 
-	public void setSpcxdz(String spcxdz)
-	{
+
+
+	public void setSpcxdz(String spcxdz) {
 		this.spcxdz = spcxdz;
 	}
 
-	public String getSycx()
-	{
+
+
+	public String getSycx() {
 		return sycx;
 	}
 
-	public void setSycx(String sycx)
-	{
+
+
+	public void setSycx(String sycx) {
 		this.sycx = sycx;
 	}
 
-	public String getSfys()
-	{
+
+
+	public String getSfys() {
 		return sfys;
 	}
 
-	public void setSfys(String sfys)
-	{
+
+
+	public void setSfys(String sfys) {
 		this.sfys = sfys;
 	}
 
-	public String getSfjbaj()
-	{
+
+
+	public String getSfzdaj() {
+		return sfzdaj;
+	}
+
+
+
+	public void setSfzdaj(String sfzdaj) {
+		this.sfzdaj = sfzdaj;
+	}
+
+
+
+	public String getSfjbaj() {
 		return sfjbaj;
 	}
 
-	public void setSfjbaj(String sfjbaj)
-	{
+
+
+	public void setSfjbaj(String sfjbaj) {
 		this.sfjbaj = sfjbaj;
 	}
 
-	public String getSffhcs()
-	{
+
+
+	public String getSffhcs() {
 		return sffhcs;
 	}
 
-	public void setSffhcs(String sffhcs)
-	{
+
+
+	public void setSffhcs(String sffhcs) {
 		this.sffhcs = sffhcs;
 	}
 
-	public String getSfwdyj()
-	{
+
+
+	public String getSfwdyj() {
 		return sfwdyj;
 	}
 
-	public void setSfwdyj(String sfwdyj)
-	{
+
+
+	public void setSfwdyj(String sfwdyj) {
 		this.sfwdyj = sfwdyj;
 	}
 
-	public int getSx()
-	{
+
+
+	public Integer getSx() {
 		return sx;
 	}
 
-	public void setSx(int sx)
-	{
+
+
+	public void setSx(Integer sx) {
 		this.sx = sx;
 	}
 
-	public int getFjsx()
-	{
+
+
+	public Integer getFjsx() {
 		return fjsx;
 	}
 
-	public void setFjsx(int fjsx)
-	{
+
+
+	public void setFjsx(Integer fjsx) {
 		this.fjsx = fjsx;
 	}
 
-	public String getLydq()
-	{
+
+
+	public String getLydq() {
 		return lydq;
 	}
 
-	public void setLydq(String lydq)
-	{
+
+
+	public void setLydq(String lydq) {
 		this.lydq = lydq;
 	}
 
-	public String getBaspt()
-	{
+
+
+	public String getBaspt() {
 		return baspt;
 	}
 
-	public void setBaspt(String baspt)
-	{
+
+
+	public void setBaspt(String baspt) {
 		this.baspt = baspt;
 	}
 
-	public String getJyaq()
-	{
+
+
+	public String getJyaq() {
 		return jyaq;
 	}
 
-	public void setJyaq(String jyaq)
-	{
+
+
+	public void setJyaq(String jyaq) {
 		this.jyaq = jyaq;
 	}
 
-	public Date getLarq()
-	{
+
+
+	public Date getLarq() {
 		return larq;
 	}
 
-	public void setLarq(Date larq)
-	{
+
+
+	public void setLarq(Date larq) {
 		this.larq = larq;
 	}
 
-	public String getLar()
-	{
+
+
+	public String getLar() {
 		return lar;
 	}
 
-	public void setLar(String lar)
-	{
+
+
+	public void setLar(String lar) {
 		this.lar = lar;
 	}
 
-	public String getBafy()
-	{
+
+
+	public String getBafy() {
 		return bafy;
 	}
 
-	public void setBafy(String bafy)
-	{
+
+
+	public void setBafy(String bafy) {
 		this.bafy = bafy;
 	}
 
-	public String getGksjg()
-	{
+
+
+	public String getGksjg() {
 		return gksjg;
 	}
 
-	public void setGksjg(String gksjg)
-	{
+
+
+	public void setGksjg(String gksjg) {
 		this.gksjg = gksjg;
 	}
 
-	public String getCbryj()
-	{
+
+
+	public String getCbryj() {
 		return cbryj;
 	}
 
-	public void setCbryj(String cbryj)
-	{
+
+
+	public void setCbryj(String cbryj) {
 		this.cbryj = cbryj;
 	}
 
-	public String getHytyj()
-	{
+
+
+	public String getHytyj() {
 		return hytyj;
 	}
 
-	public void setHytyj(String hytyj)
-	{
+
+
+	public void setHytyj(String hytyj) {
 		this.hytyj = hytyj;
 	}
 
-	public String getTzhyj()
-	{
+
+
+	public String getTzhyj() {
 		return tzhyj;
 	}
 
-	public void setTzhyj(String tzhyj)
-	{
+
+
+	public void setTzhyj(String tzhyj) {
 		this.tzhyj = tzhyj;
 	}
 
-	public String getYzhyj()
-	{
+
+
+	public String getYzhyj() {
 		return yzhyj;
 	}
 
-	public void setYzhyj(String yzhyj)
-	{
+
+
+	public void setYzhyj(String yzhyj) {
 		this.yzhyj = yzhyj;
 	}
 
-	public String getSwhyj()
-	{
+
+
+	public String getSwhyj() {
 		return swhyj;
 	}
 
-	public void setSwhyj(String swhyj)
-	{
+
+
+	public void setSwhyj(String swhyj) {
 		this.swhyj = swhyj;
 	}
 
-	public Date getJarq()
-	{
+
+
+	public Date getJarq() {
 		return jarq;
 	}
 
-	public void setJarq(Date jarq)
-	{
+
+
+	public void setJarq(Date jarq) {
 		this.jarq = jarq;
 	}
 
-	public String getJafs()
-	{
+
+
+	public String getJafs() {
 		return jafs;
 	}
 
-	public void setJafs(String jafs)
-	{
+
+
+	public void setJafs(String jafs) {
 		this.jafs = jafs;
 	}
 
-	public String getJayy()
-	{
+
+
+	public String getJayy() {
 		return jayy;
 	}
 
-	public void setJayy(String jayy)
-	{
+
+
+	public void setJayy(String jayy) {
 		this.jayy = jayy;
 	}
 
-	public String getJayydm()
-	{
+
+
+	public String getJayydm() {
 		return jayydm;
 	}
 
-	public void setJayydm(String jayydm)
-	{
+
+
+	public void setJayydm(String jayydm) {
 		this.jayydm = jayydm;
 	}
 
-	public String getBz()
-	{
+
+
+	public String getBz() {
 		return bz;
 	}
 
-	public void setBz(String bz)
-	{
+
+
+	public void setBz(String bz) {
 		this.bz = bz;
 	}
 
-	public Date getSxrq()
-	{
+
+
+	public Date getSxrq() {
 		return sxrq;
 	}
 
-	public void setSxrq(Date sxrq)
-	{
+
+
+	public void setSxrq(Date sxrq) {
 		this.sxrq = sxrq;
 	}
 
-	public String getGdbz()
-	{
+
+
+	public String getGdbz() {
 		return gdbz;
 	}
 
-	public void setGdbz(String gdbz)
-	{
+
+
+	public void setGdbz(String gdbz) {
 		this.gdbz = gdbz;
 	}
 
-	public String getAjzt()
-	{
+
+
+	public String getAjzt() {
 		return ajzt;
 	}
 
-	public void setAjzt(String ajzt)
-	{
+
+
+	public void setAjzt(String ajzt) {
 		this.ajzt = ajzt;
 	}
 
-	public String getBycxdz()
-	{
+
+
+	public String getBycxdz() {
 		return bycxdz;
 	}
 
-	public void setBycxdz(String bycxdz)
-	{
+
+
+	public void setBycxdz(String bycxdz) {
 		this.bycxdz = bycxdz;
 	}
 
-	public String getAjwsqk()
-	{
+
+
+	public String getAjwsqk() {
 		return ajwsqk;
 	}
 
-	public void setAjwsqk(String ajwsqk)
-	{
+
+
+	public void setAjwsqk(String ajwsqk) {
 		this.ajwsqk = ajwsqk;
 	}
 
-	public Date getGdrq()
-	{
+
+
+	public Date getGdrq() {
 		return gdrq;
 	}
 
-	public void setGdrq(Date gdrq)
-	{
+
+
+	public void setGdrq(Date gdrq) {
 		this.gdrq = gdrq;
 	}
 
-	public int getGdxlh()
-	{
+
+
+	public Integer getGdxlh() {
 		return gdxlh;
 	}
 
-	public void setGdxlh(int gdxlh)
-	{
+
+
+	public void setGdxlh(Integer gdxlh) {
 		this.gdxlh = gdxlh;
 	}
 
-	public Date getCbrbprq()
-	{
+
+
+	public Date getCbrbprq() {
 		return cbrbprq;
 	}
 
-	public void setCbrbprq(Date cbrbprq)
-	{
+
+
+	public void setCbrbprq(Date cbrbprq) {
 		this.cbrbprq = cbrbprq;
 	}
 
-	public String getCjar()
-	{
+
+
+	public String getCjar() {
 		return cjar;
 	}
 
-	public void setCjar(String cjar)
-	{
+
+
+	public void setCjar(String cjar) {
 		this.cjar = cjar;
 	}
 
-	public Date getSpzsqrq()
-	{
+
+
+	public Date getSpzsqrq() {
 		return spzsqrq;
 	}
 
-	public void setSpzsqrq(Date spzsqrq)
-	{
+
+
+	public void setSpzsqrq(Date spzsqrq) {
 		this.spzsqrq = spzsqrq;
 	}
 
-	public Date getHythyrq()
-	{
+
+
+	public Date getHythyrq() {
 		return hythyrq;
 	}
 
-	public void setHythyrq(Date hythyrq)
-	{
+
+
+	public void setHythyrq(Date hythyrq) {
 		this.hythyrq = hythyrq;
 	}
 
-	public Date getTzsqrq()
-	{
+
+
+	public Date getTzsqrq() {
 		return tzsqrq;
 	}
 
-	public void setTzsqrq(Date tzsqrq)
-	{
+
+
+	public void setTzsqrq(Date tzsqrq) {
 		this.tzsqrq = tzsqrq;
 	}
 
-	public Date getYzsqrq()
-	{
+
+
+	public Date getYzsqrq() {
 		return yzsqrq;
 	}
 
-	public void setYzsqrq(Date yzsqrq)
-	{
+
+
+	public void setYzsqrq(Date yzsqrq) {
 		this.yzsqrq = yzsqrq;
 	}
 
-	public Date getSwhjdrq()
-	{
+
+
+	public Date getSwhjdrq() {
 		return swhjdrq;
 	}
 
-	public void setSwhjdrq(Date swhjdrq)
-	{
+
+
+	public void setSwhjdrq(Date swhjdrq) {
 		this.swhjdrq = swhjdrq;
 	}
 
-	public Date getYzqfrq()
-	{
+
+
+	public Date getYzqfrq() {
 		return yzqfrq;
 	}
 
-	public void setYzqfrq(Date yzqfrq)
-	{
+
+
+	public void setYzqfrq(Date yzqfrq) {
 		this.yzqfrq = yzqfrq;
 	}
 
-	public String getSlqk()
-	{
+
+
+	public String getSlqk() {
 		return slqk;
 	}
 
-	public void setSlqk(String slqk)
-	{
+
+
+	public void setSlqk(String slqk) {
 		this.slqk = slqk;
 	}
 
-	public String getSslx()
-	{
+
+
+	public String getSslx() {
 		return sslx;
 	}
 
-	public void setSslx(String sslx)
-	{
+
+
+	public void setSslx(String sslx) {
 		this.sslx = sslx;
 	}
 
-	public String getSfgs()
-	{
+
+
+	public String getSfgs() {
 		return sfgs;
 	}
 
-	public void setSfgs(String sfgs)
-	{
+
+
+	public void setSfgs(String sfgs) {
 		this.sfgs = sfgs;
 	}
 
-	public String getFhcsyy()
-	{
+
+
+	public String getFhcsyy() {
 		return fhcsyy;
 	}
 
-	public void setFhcsyy(String fhcsyy)
-	{
+
+
+	public void setFhcsyy(String fhcsyy) {
 		this.fhcsyy = fhcsyy;
 	}
 
-	public String getGpyy()
-	{
+
+
+	public String getGpyy() {
 		return gpyy;
 	}
 
-	public void setGpyy(String gpyy)
-	{
+
+
+	public void setGpyy(String gpyy) {
 		this.gpyy = gpyy;
 	}
 
-	public String getSfzscq()
-	{
+
+
+	public String getSfzscq() {
 		return sfzscq;
 	}
 
-	public void setSfzscq(String sfzscq)
-	{
+
+
+	public void setSfzscq(String sfzscq) {
 		this.sfzscq = sfzscq;
 	}
 
-	public String getSwlx()
-	{
+
+
+	public String getSwlx() {
 		return swlx;
 	}
 
-	public void setSwlx(String swlx)
-	{
+
+
+	public void setSwlx(String swlx) {
 		this.swlx = swlx;
 	}
 
 
 
-	public String getSfzdaj()
-	{
-		return sfzdaj;
-	}
-
-	public void setSfzdaj(String sfzdaj)
-	{
-		this.sfzdaj = sfzdaj;
-	}
-
 	public String getPsycy() {
 		return psycy;
 	}
+
+
 
 	public void setPsycy(String psycy) {
 		this.psycy = psycy;
 	}
 
+
+
 	public String getSfxess() {
 		return sfxess;
 	}
+
+
 
 	public void setSfxess(String sfxess) {
 		this.sfxess = sfxess;
 	}
 
+	
 	
 	
 	
