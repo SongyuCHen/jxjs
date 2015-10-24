4ccedc8abb9c5333be9d3585f99b039eb12c2166
SongyuCHen <363278429@qq.com>
2015-10-24 09:32:09 +0800

    Add by csy

---
+++ jxjs/src/nju/software/jxjs/dao/XtglDmbDao.java
@@ -0,0 +1,37 @@
+package nju.software.jxjs.dao;
+
+import java.util.List;
+
+import nju.software.jxjs.model.PubDmb;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class XtglDmbDao extends BaseDao {
+	@SuppressWarnings("unchecked")
+	public List<PubDmb> getDmbByLbbh(String lbbh){
+		String hql = "from PubDmb dmb where dmb.lbbh=?";
+		List<PubDmb> dmbs =  (List<PubDmb>) getHibernateTemplate().find(hql, new Object[]{lbbh});
+		return dmbs;
+	}
+	@SuppressWarnings("unchecked")
+	public PubDmb getDmbByLbbhAndDmms(String lbbh,String dmms){
+		String hql = "from PubDmb dmb where dmb.lbbh=? and dmb.dmms=?";
+		List<PubDmb> dmbs =  (List<PubDmb>) getHibernateTemplate().find(hql, new Object[]{lbbh,dmms});
+		if(dmbs != null && dmbs.size() > 0)
+			return dmbs.get(0);
+		else
+			return null;
+	}
+	@SuppressWarnings("unchecked")
+	public PubDmb getDmbByLbbhAndDmbh(String lbbh,String dmbh){
+		String hql = "from PubDmb dmb where dmb.lbbh=? and dmb.dmbh=?";
+		List<PubDmb> dmbs =  (List<PubDmb>) getHibernateTemplate().find(hql, new Object[]{lbbh,dmbh});
+		if(dmbs != null && dmbs.size() > 0)
+			return dmbs.get(0);
+		else
+			return null;
+	}
+	
+
+}
