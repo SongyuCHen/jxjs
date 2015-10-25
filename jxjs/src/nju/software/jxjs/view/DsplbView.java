ff0cb495640c7890fbe22286184d9acbec09e080
lucyTheSlayer <saber_zyl@qq.com>
2015-10-25 17:31:18 +0800

    ~

---
+++ jxjs/src/nju/software/jxjs/view/DsplbView.java
@@ -0,0 +1,38 @@
+package nju.software.jxjs.view;
+
+import java.io.Serializable;
+
+public class DsplbView implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5010992946598417230L;
+	
+	//##############业务逻辑需要!!!######################
+	//减刑假释编号
+	private int jxjsbh;
+	//##################################################
+	
+	
+	
+	
+	
+	
+	
+	//########################页面显示相关！！！！####################
+	//序号    略
+	//原审案号
+	private String ysah;
+	//当事人
+	private String dsr;
+	//生效法院
+	private String sxfy;
+	//申请类型
+	private String sqlx;
+	//申请时间
+	private String sqsj;
+	//申请次数
+	private int sqcs;
+	//####################################################
+}
